4237dfe2f220b8e48ce9d7ef118b17e37c336e19
James Newman <jamesmnewman1@gmail.com>
2015-01-04 22:58:19 -0800

    good

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+
+var app = express()
+app.use(bodyParser.json())
+
+app.get('/api/posts', function (req, res) {
+	res.json([
+	{
+		username: 'Jimmy_Westside',
+		body: 'node and such'
+	}
+ ])
+})
+
+app.post('/api/posts', function (req, res) {
+	console.log('Post Received!')
+	console.log(req.body.username)
+	console.log(req.body.body)
+	res.send(201)
+})
+
+app.listen(3000, function () {
+	console.log("Listening on", 3000)
+})
